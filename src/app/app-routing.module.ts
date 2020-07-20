5225cf70da65ed91465aab22d8eb3fcaa3156711
renato_luzuriaga <renatoluzuriaga@gmail.com>
2020-07-20 20:30:35 +0200

    Creo los componentes del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path:'',
+  redirectTo:'home',
+  pathMatch:'full'
+},
+{ path: 'home', loadChildren: () => import('./component/pages/home/home.module').then(m => m.HomeModule) },
+{ path: 'character-list', loadChildren: () => import('./component/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) },
+{ path: 'character-details/:id', loadChildren: () => import('./component/pages/characters/character-details/character-details.module').then(m => m.CharacterDetailsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/pages/characters/character-details/character-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-character-details',
+  templateUrl: './character-details.component.html',
+  styleUrls: ['./character-details.component.scss']
+})
+export class CharacterDetailsComponent implements OnInit {
+
+  /*
+  TODO:
+  
+  */
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
