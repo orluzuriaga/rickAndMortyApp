2257f2467dc90045776c3a6969bc5b6164747be9
renato_luzuriaga <renatoluzuriaga@gmail.com>
2020-07-24 13:10:18 +0200

    Refatorizo el componente character-list y character-details -> character-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FormSearchComponent } from './shared/components/form-search/form-search.component';
 import { HttpClientModule } from '@angular/common/http';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FormSearchComponent
+    FormSearchComponent,
+
+
   ],
   imports: [
     BrowserModule,
--- src/app/component/pages/characters/characters.module.ts
+++ src/app/component/pages/characters/characters.module.ts
@@ -3,16 +3,17 @@ import { CommonModule } from '@angular/common';
 import { CharacterDetailsComponent } from './character-details/character-details.component';
 import { CharacterListComponent } from './character-list/character-list.component';
 import { RouterEvent, RouterModule } from '@angular/router';
+import { CharacterComponent } from './character-card/character/character.component';
 
 
 const MYCOMPONENTS = [CharacterDetailsComponent,CharacterListComponent]
 @NgModule({
-  declarations: [ ...MYCOMPONENTS],
+  declarations: [ ...MYCOMPONENTS, CharacterComponent],
   imports: [
     CommonModule,
     RouterModule
 
   ],
-  exports:[...MYCOMPONENTS]
+  exports:[...MYCOMPONENTS,CharacterComponent]
 })
 export class CharactersModule { }
